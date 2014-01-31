b714d7f52ebe6d2e2a3710bc222436ae43b3cc52
Eric Moyer <eric_moyer@yahoo.com>
2014-01-31 16:24:03 -0500

    Added code for creating a dataset from many instances of the shifted 1 process.

---
+++ MSThesis/shift_1_pca_example/shifted_1_dataset.m
@@ -0,0 +1,45 @@
+function [dataset, noiseless] = shifted_1_dataset( num_rows, shifted_1_side_size, seed )
+% Return a matrix whose rows are random shifted_1 instances
+% 
+% Does not affect the random number generator (restores the seed after it
+% finishes)
+% -------------------------------------------------------------------------
+% Input
+% -------------------------------------------------------------------------
+% num_rows - (non-negative integer) the number of rows in the matrix to
+%     return
+%
+% shifted_1_side_size - (positive integer) the length of a side of the
+%     matrix into which the shifted 1s are embedded
+%
+% seed - (integer) the seed to use for the random number generator (must be
+%   acceptable input to rng(sd))
+% -------------------------------------------------------------------------
+% Output
+% -------------------------------------------------------------------------
+% dataset - (matrix) a num_rows x (shifted_1_side_size^2) matrix whose rows
+%   are independently chosen shifted_1 instances and whose columns are 0 or
+%   1 entries of the shifted_1 matrices
+%
+% noiseless - (matrix) same shifted_1 instances as dataset but without the
+%   noise
+
+assert(num_rows == round(num_rows));
+assert(num_rows >= 0)
+assert(shifted_1_side_size > 0);
+assert(shifted_1_side_size == round(shifted_1_side_size));
+assert(seed==round(seed));
+
+oldseed = rng(seed);
+
+dataset = zeros(num_rows, shifted_1_side_size ^ 2);
+noiseless = dataset;
+for i=1:num_rows
+    a = randi([-3, shifted_1_side_size-3]);
+    b = randi([-3, shifted_1_side_size-3]);
+    [dataset(i,:), noiseless(i,:)] = shifted_1(a,b,shifted_1_side_size,0.05);
+end
+
+rng(oldseed);
+end
+
