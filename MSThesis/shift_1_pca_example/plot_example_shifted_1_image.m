757cb53bb520eb37f4b852d8d56dbcadd0a699e6
Eric Moyer <eric_moyer@yahoo.com>
2014-01-31 12:52:10 -0500

    Made the example image generator save it to a file and added that file to the repo (just because it would be a pain to need matlab to regenerate the figure just to print a copy of the thesis). I will include all figures in the repo so that a check-out can easily be printed.

--- MSThesis/shift_1_pca_example/plot_example_shifted_1_image.m
+++ MSThesis/shift_1_pca_example/plot_example_shifted_1_image.m
@@ -1,8 +1,12 @@
 function plot_example_shifted_1_image( )
 % Plots an example shifted 1 image and saves it to a file
 
+oldseed = rng(12341248);
+
 w=25; [~,m]=shifted_1(9,9,w,0.05); image(m*60)
 title('Example Shifted 1 Image');
+print(1, 'shifted_1_image_example.pdf', '-dpdf');
 
+rng(oldseed);
 end
 
