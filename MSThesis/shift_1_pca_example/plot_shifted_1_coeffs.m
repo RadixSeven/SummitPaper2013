ce097dbd4d00c28f006c9cef9d33557a251b8378
Eric Moyer <eric_moyer@yahoo.com>
2014-02-01 17:53:47 -0500

    Modified the coefficient plotter to plot in rows of 10 and plotted the coefficients derived from 50,000 bit-flip and gaussian 20x20 examples.

--- MSThesis/shift_1_pca_example/plot_shifted_1_coeffs.m
+++ MSThesis/shift_1_pca_example/plot_shifted_1_coeffs.m
@@ -31,28 +31,50 @@ green = round(green * 255);
 fid = fopen(filename,'w');
 
 % Print header
-fprintf(fid, 'P3 %d %d %d\n', (width+1)*num_components, width, 255);
+num_master_rows = ceil(num_components/10);
+fprintf(fid, 'P3 %d %d %d\n', (width+1)*10-1, (width+1)*num_master_rows, 255);
 
-% Print image data (no line over 70 chars as per standard)
+% Create blank image. pict(:,:,1) is red. pict(:,:,2) is green, etc
+% First coord is x coord, second is y (rather than standard matrix indices)
+pict = zeros((width+1)*10-1, (width+1)*num_master_rows, 3);
+
+% Make the picture blue where it is not overwritten
+pict(:,:,3) = 255;
+
+% Overwrite the picture data in the areas where there is a component image
+for component = 1:num_components
+    % 0 based x and y coordinates of the sub-image
+    subimage_y = floor((component-1)/10);
+    subimage_x = mod(component-1, 10);
+    
+    % 1 based indices into the pict for the upper-left pixel of the current
+    % subimage - the ul pixel is accessed at pict(pict_ul_x, pict_ul_y)
+    pict_ul_x = subimage_x*(width+1)+1;
+    pict_ul_y = subimage_y*(width+1)+1;
+    
+    for pixel = 1:size(coeff,1)
+        pixel_y = floor((pixel-1)/width);
+        pixel_x = mod(pixel-1, width);
+        
+        pict(pict_ul_x+pixel_x, pict_ul_y+pixel_y,:)=[...
+            red(pixel, component) green(pixel, component) 0];
+    end
+end
+
+% Write out the pixels
 cur_char = 0;
-for row=0:width-1
-    offset = row*width;
-    for component = 1:num_components
-        for col=1:width
-            fprintf(fid,'%4d%4d%4d', red(offset+col, component), green(offset+col, component), 0);
-            cur_char = cur_char + 12;
-            if(cur_char >= (70-12))
-                fprintf(fid, '\n');
-                cur_char = 0;
-            end
+for y = 1:size(pict,2)
+    for x = 1:size(pict,1)
+        fprintf(fid,'%4d%4d%4d', pict(x,y,1), pict(x,y,2), pict(x,y,3));
+        cur_char = cur_char + 12;
+        if(cur_char >= (70-12))
+            fprintf(fid, '\n');
+            cur_char = 0;
         end
-        fprintf(fid, ' 0 0 255\n');
-        cur_char = 0;
     end
     fprintf(fid, '\n');
     cur_char = 0;
 end
-
 
 fclose(fid);
 end
